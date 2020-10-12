f883e467f8bcd4afae116b9764268dfd089af64a
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2020-10-12 18:37:55 -0400

    chore:git quirks

--- src/Popup/Popup.tsx
+++ src/Popup/Popup.tsx
@@ -1,67 +1,29 @@
 import React from "react";
-import { browser, Tabs } from "webextension-polyfill-ts";
+import { useQuery } from "react-query";
+import { DataService } from "../common/DataService";
 
-import "./styles.scss";
+const Popup = () => {
+  const { data, isLoading, isError } = useQuery(
+    "projects",
+    DataService.getProjects
+  );
 
-function openWebPage(url: string): Promise<Tabs.Tab> {
-  return browser.tabs.create({ url });
-}
+  if (isError) {
+    return <div>Error</div>;
+  }
 
-const Popup: React.FC = () => {
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <section id="popup">
-      <h2>WEB-EXTENSION-STARTER</h2>
-      <button
-        id="options__button"
-        type="button"
-        onClick={(): Promise<Tabs.Tab> => {
-          return openWebPage("options.html");
-        }}
-      >
-        Options Page
-      </button>
-      <div className="links__holder">
-        <ul>
-          
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  "https://github.com/abhijithvijayan/web-extension-starter"
-                );
-              }}
-            >
-              GitHub (me saisandeepvaddi)
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  "https://github.com/abhijithvijayan/web-extension-starter"
-                );
-              }}
-            >
-              GitHub (upstream author abhijithvijayan)
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  "https://www.buymeacoffee.com/abhijithvijayan"
-                );
-              }}
-            >
-              Buy Me A Coffee (to the upstream author)
-            </button>
-          </li>
-        </ul>
-      </div>
-    </section>
+    <div style={{ width: 500 }}>
+      <ul>
+        {data?.map((project) => (
+          <li key={project._id}>{project.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
